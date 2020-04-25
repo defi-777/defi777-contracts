3bb59e3a4b4d7c2c2dcd4450ad9b671096faff3f
David Mihal <david@dmihal.com>
2020-04-25 12:29:03 -0300

    Uniswap 777->777 test

--- test/uniswap.js
+++ test/uniswap.js
@@ -66,5 +66,32 @@ group('Uniswap', (accounts) => {
       .to.equal((parseInt(startingBalance) + parseInt(toWei('1', 'ether')) - ethSpentOnGas).toString());
   });
 
-  it('Should swap a 777 token for another token');
+  it('Should swap a 777 token for another token', async () => {
+    const factory = await WrapperFactory.new();
+
+    const token1 = await TestERC20.new();
+    const token2 = await TestERC20.new();
+
+    await factory.create(token1.address);
+    await factory.create(token2.address);
+    const wrapper1Address = await factory.getWrapper(token1.address);
+    const wrapper2Address = await factory.getWrapper(token2.address);
+    const wrapper1 = await Wrapped777.at(wrapper1Address);
+    const wrapper2 = await Wrapped777.at(wrapper2Address);
+
+    const uniswapRouter = await TestUniswapRouter.new();
+    await token1.transfer(uniswapRouter.address, toWei('100', 'ether'));
+
+    await token2.approve(wrapper2Address, toWei('10', 'ether'));
+    await wrapper2.wrap(toWei('10', 'ether'));
+    await wrapper2.transfer(user, toWei('2', 'ether'));
+
+    const uniswapFactory = await UniswapWrapperFactory.new(uniswapRouter.address);
+    await uniswapFactory.createExchange(wrapper1.address);
+    const exchangeAddress = await uniswapFactory.getWrapper(wrapper1.address);
+    const exchange = await UniswapWrapper.at(exchangeAddress);
+
+    await wrapper2.transfer(exchangeAddress, toWei('1', 'ether'), { from: user });
+    expect(await str(wrapper1.balanceOf(user))).to.equal(toWei('1', 'ether'));
+  });
 });
