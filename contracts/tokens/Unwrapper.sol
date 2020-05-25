8c5e72a4f2aa1e63648f8e7911dc3fecabb3a083
David Mihal <david@dmihal.com>
2020-05-24 22:27:56 -0300

    Fix import

--- contracts/tokens/Unwrapper.sol
+++ contracts/tokens/Unwrapper.sol
@@ -1,7 +1,7 @@
 pragma solidity >=0.6.2 <0.7.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "./Wrapped777.sol";
+import "./IWrapped777.sol";
 import "../Receiver.sol";
 
 contract Unwrapper is Receiver {
