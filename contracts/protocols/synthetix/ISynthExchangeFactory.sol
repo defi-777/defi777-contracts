013db1140e3742f825626a76e62ff5aa2dfd7173
David Mihal <david@dmihal.com>
2020-11-22 22:33:46 +0000

    Remove synthetix

--- contracts/protocols/synthetix/ISynthExchangeFactory.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity >=0.6.2 <0.7.0;
-
-interface ISynthExchangeFactory {
-  function snx() external view returns (address);
-  function uniswapRouter() external view returns (address);
-  function nextWrapper() external view returns (address);
-}
