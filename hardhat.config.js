c91603e33a311a17f28c2adeebd599e9680ac0ed
David Mihal <david@dmihal.com>
2020-11-29 15:50:11 +0000

    Create wrappers task

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,9 +5,31 @@ require("hardhat-deploy");
 
 task("deployments", "Prints all deployment addresses", async function(_, { deployments }) {
   Object.entries(await deployments.all()).map(([name, metadata]) => {
-    console.log(`${name}: ${metadata.address} (${metadata.transactionHash} : ${metadata.receipt.blockNumber})`)
+    console.log(`${name}: ${metadata.address} (${metadata.transactionHash} : ${metadata.receipt && metadata.receipt.blockNumber})`)
   });
 });
+
+const SCAN_SIZE = 10000;
+
+task("wrappers", "Prints all deployed wrappers", async function(_, { deployments }) {
+  const WrapperFactory = artifacts.require('WrapperFactory');
+  const ERC20 = artifacts.require('ERC20');
+
+  const wrapperFactoryDeployment = await deployments.get('WrapperFactory');
+  const wrapperFactory = await WrapperFactory.at(wrapperFactoryDeployment.address);
+  console.log('== WrapperFactory ==\n====================');
+
+  const now = await web3.eth.getBlockNumber();
+  for (let block = wrapperFactoryDeployment.receipt.blockNumber; block < now; block += SCAN_SIZE) {
+    const events = await wrapperFactory.getPastEvents('WrapperCreated', { fromBlock: block, toBlock: block + SCAN_SIZE });
+    for (event of events) {
+      const tokenAddress = event.returnValues.token;
+      const wrapperAddress = await wrapperFactory.calculateWrapperAddress(tokenAddress);
+      const token = await ERC20.at(tokenAddress);
+      console.log(`- ${await token.name()}: ${tokenAddress} => ${wrapperAddress}`);
+    }
+  }
+})
 
 module.exports = {
   networks: {
