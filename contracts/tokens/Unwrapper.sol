7a2ab94a7eab8800cc728f20897406966a70a976
David Mihal <david@dmihal.com>
2020-11-29 18:45:42 +0000

    Add unwrapper to ENS

--- contracts/tokens/Unwrapper.sol
+++ contracts/tokens/Unwrapper.sol
@@ -1,10 +1,11 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.6.2 <0.7.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./IWrapped777.sol";
 import "../Receiver.sol";
+import "../ens/ReverseENS.sol";
 
-contract Unwrapper is Receiver {
+contract Unwrapper is Receiver, ReverseENS {
   function _tokensReceived(IERC777 _token, address from, uint256 amount, bytes memory /*data*/) internal override {
     IWrapped777(address(_token)).unwrapTo(amount, from);
   }
