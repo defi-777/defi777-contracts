c93d52fe99b84e7b29003f6033873eafc758d960
David Mihal <david@dmihal.com>
2020-12-04 19:25:47 +0000

    Update deployments

--- deploy/Aave.js
+++ deploy/Aave.js
@@ -2,7 +2,7 @@ const aaveContracts = {
   // '1': {
   // },
   '42': {
-    addressProvider: '0xbF3f2372E6073FCf31d16212A471b36cDb8D6071',
+    addressProvider: '0x652B2937Efd0B5beA1c8d54293FC1289672AFC6b',
     weth: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
     aWeth: '0xe2735Adf49D06fBC2C09D9c0CFfbA5EF5bA35649',
     aTokens: [
